618c44c31b7481e98c400cf7feec15ca34179268
rick0 <rick0@DESKTOP-9PL48K8>
2024-03-21 18:55:25 -0300

    Add project files.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número para verificar se pertence à sequência de Fibonacci: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        bool pertence = VerificarFibonacci(numero);
+
+        if (pertence)
+        {
+            Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} NÃO pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool VerificarFibonacci(int num)
+    {
+        int a = 0;
+        int b = 1;
+
+        while (b < num)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return b == num;
+    }
+}
---
+++ Exercicio5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite uma palavra ou frase para inverter os caracteres: ");
+        string texto = Console.ReadLine();
+
+        string stringInvertida = InverterString(texto);
+
+        Console.WriteLine("String invertida: " + stringInvertida);
+    }
+
+    static string InverterString(string str)
+    {
+        // transforma a string em um Array de caracteres
+        char[] arrayCaracteres = str.ToCharArray();
+
+        //  indice do último caractere na string
+        int final = arrayCaracteres.Length - 1;
+
+        // loop que inverte os caracteres
+        for (int inicio = 0; inicio < final; inicio++, final--)
+        {
+            // variavel temporaria para guardar a letra que sera invertida
+            char temp = arrayCaracteres[inicio];
+            // primeira letra da string é substituida pela ultima
+            arrayCaracteres[inicio] = arrayCaracteres[final];
+            // o local do ultimo caractere recebe o caracter inicial que foi guardado na variavel temporaria
+            arrayCaracteres[final] = temp;
+        }
+
+        // converte o array de caracteres de volta para uma string
+        return new string(arrayCaracteres);
+    }
+}
